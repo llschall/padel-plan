29080af00a9279c0b51e15a02b96446c6b53db04
llschall <llschall@gmx.com>
2025-07-05 18:57:33 +0200

    [Combinator] Test consumer

--- app/src/main/java/org/llschall/padel/combinator/Sequence.java
+++ app/src/main/java/org/llschall/padel/combinator/Sequence.java
@@ -1,5 +1,8 @@
 package org.llschall.padel.combinator;
 
+import java.util.Arrays;
+import java.util.Objects;
+
 class Sequence {
 
     final String[] data;
@@ -10,5 +13,16 @@ class Sequence {
 
     Sequence freeze() {
         return new Sequence(data.clone());
+    }
+
+    @Override
+    public boolean equals(Object o) {
+        if (!(o instanceof Sequence sequence)) return false;
+        return Objects.deepEquals(data, sequence.data);
+    }
+
+    @Override
+    public int hashCode() {
+        return Arrays.hashCode(data);
     }
 }
--- app/src/test/java/org/llschall/padel/combinator/EnumeratorTest.java
+++ app/src/test/java/org/llschall/padel/combinator/EnumeratorTest.java
@@ -11,51 +11,82 @@ public class EnumeratorTest {
     @Test
     void testEnumerator0() {
         Enumerator enumerator = new Enumerator();
+        TestConsumer consumer = new TestConsumer(List.of(
+                new Sequence("A"),
+                new Sequence("C", "B")
+        ));
         enumerator.process(new Sequence[]{
                 new Sequence("A"),
                 new Sequence("B", "C"),
-        }, new TestConsumer());
+        }, consumer);
         Assertions.assertEquals(2, enumerator.combinator.count);
+        Assertions.assertTrue(consumer.found);
     }
 
     @Test
     void testEnumerator1() {
         Enumerator enumerator = new Enumerator();
+        TestConsumer consumer = new TestConsumer(List.of(
+                new Sequence("B", "A", "C"),
+                new Sequence("F", "E", "D"),
+                new Sequence("G", "H", "I")
+        ));
         enumerator.process(new Sequence[]{
                 new Sequence("A", "B", "C"),
                 new Sequence("D", "E", "F"),
                 new Sequence("G", "H", "I"),
-        }, new TestConsumer());
+        }, consumer);
         Assertions.assertEquals(216, enumerator.combinator.count);
+        Assertions.assertTrue(consumer.found);
     }
 
     @Test
     void testEnumerator2() {
         Enumerator enumerator = new Enumerator();
+        TestConsumer consumer = new TestConsumer(List.of(
+                new Sequence("A", "C", "B"),
+                new Sequence("D", "E"),
+                new Sequence("I", "H", "G", "F")
+        ));
         enumerator.process(new Sequence[]{
                 new Sequence("A", "B", "C"),
                 new Sequence("D", "E"),
                 new Sequence("F", "G", "H", "I"),
-        }, new TestConsumer());
+        }, consumer);
         Assertions.assertEquals(288, enumerator.combinator.count);
+        Assertions.assertTrue(consumer.found);
     }
 
     @Test
     void testEnumerator3() {
         Enumerator enumerator = new Enumerator();
+        TestConsumer consumer = new TestConsumer(List.of(
+                new Sequence("A", "B", "C", "D", "E", "I", "G", "H", "F", "J"),
+                new Sequence("C", "B", "A")
+        ));
         enumerator.process(new Sequence[]{
                 new Sequence("A", "B", "C", "D", "E", "F", "G", "H", "I", "J"),
                 new Sequence("A", "B", "C"),
-        }, new TestConsumer());
+        }, consumer);
         Assertions.assertEquals(21772800, enumerator.combinator.count);
+        Assertions.assertTrue(consumer.found);
     }
 }
 
 class TestConsumer implements Consumer<List<Sequence>> {
 
+    List<Sequence> expected;
+    boolean found = false;
+
+    public TestConsumer(List<Sequence> expected) {
+        this.expected = expected;
+    }
+
     @Override
     public void accept(List<Sequence> sequences) {
-        // This is a no-op consumer for testing purposes.
-        // In a real scenario, you might want to process the sequences further.
+        for (int i = 0; i < sequences.size(); i++) {
+            if (!sequences.get(i).equals(expected.get(i))) return;
+        }
+        found = true;
     }
 }
